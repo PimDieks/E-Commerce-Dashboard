dbb0df89fb38850adc93183b3c0a6a89138b4394
JarnoPeeters <info@insideweb.nl>
2017-03-24 12:18:06 +0100

    Plugin available in WordPress

---
+++ e-commerce-dashboard.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: E-commerce Dashboard
+ * Plugin URI: 
+ * Description: Manage your e-commerce with management dashboards.
+ * Version: 0.1.1
+ * Author: Jarno Peeters & Pim Dieks
+ * Author URI:
+ * License: GPL2
+ */
