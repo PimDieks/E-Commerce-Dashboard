1c094c7de36316a6bc77203f68d011e4422ccc28
JarnoPeeters <info@insideweb.nl>
2017-04-06 12:41:55 +0200

    Added Dashboards en Settings page

--- e-commerce-dashboards.php
+++ e-commerce-dashboards.php
@@ -9,17 +9,26 @@
  * License: GPL2
  */
 
-// Add Page
-add_action("admin_menu", "addMenu");
+// Add Menu
 function addMenu() {
-	add_menu_page("Dashboards", "Dashboards", 4, "dashboards", "dashboards");
-	add_submenu_page("Settings", "Settings", "Settings", 4, "settings", "settings");
+	add_menu_page("Dashboards", "Dashboards", 4, "dashboards", "dashboards", "dashicons-screenoptions");
+	add_submenu_page("dashboards", "Settings", "Settings", 4, "settings", "settings");
 }
+add_action("admin_menu", "addMenu");
 
+// Page Dashboards
 function dashboards() {
-	echo "<h1 class='wp-heading-inline'>E-Commerce Dashboards</h1>";
+	echo "<h1>E-Commerce Dashboards</h1>";
+	echo "<p>Welcome to E-Commerce Dashboards. Your all-in-one dashboard plugin!</p>";
 }
 
+// Page Settings
 function settings() {
-	echo "Settings";
+	echo "<h1>Settings</h1>";
+	echo "<p>Select the dashboards you want to see in your overview.</p>";
+	echo "<form>
+			<label><input type='checkbox' name='product-overview'>Product Overview</label><br><br>
+			<label><input type='checkbox' name='customer-overview'>Customer Overview</label><br><br>
+			<input type='submit' name='submit' value='Save' class='button button-primary button-large'>
+		</form>";
 }
