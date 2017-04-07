09a161d6fb317024eaa084874e9cfb98042e34de
Pim Dieks <pimdieks@Pims-MacBook-Pro.local>
2017-04-07 12:02:49 +0200

    update echo

--- e-commerce-dashboards.php
+++ e-commerce-dashboards.php
@@ -32,23 +32,7 @@ function dashboards() {
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Product');
         data.addRows([
-          <?php include 'conn_database.php';
-
-	$sql = "SELECT post_title 
-			FROM wp_posts
-			WHERE post_type = 'product'
-			AND post_status = 'publish'
-			ORDER BY post_title";
-	$result = $conn->query($sql);
-
-	if ($result->num_rows > 0) {
-	    // output data of each row
-	    while($row = $result->fetch_assoc()) {
-	        echo "['" . $row['post_title'] . "'],";
-	    }
-	} else {
-	    echo "0 results";
-	} ?>
+          <?php include 'product_echo.php';?>
         ]);
 
         var table = new google.visualization.Table(document.getElementById('table_div'));
---
+++ product_echo.php
@@ -0,0 +1,17 @@
+          <?php include 'conn_database.php';
+
+	$sql = "SELECT post_title 
+			FROM wp_posts
+			WHERE post_type = 'product'
+			AND post_status = 'publish'
+			ORDER BY post_title";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "['" . $row['post_title'] . "'],";
+	    }
+	} else {
+	    echo "0 results";
+	} ?>
