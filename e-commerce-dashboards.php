e24441420f4bc6ab22bacfd9181a6ba7f24b5485
JarnoPeeters <info@insideweb.nl>
2017-04-07 12:15:00 +0200

    Add dashboard to startscherm WordPress

--- e-commerce-dashboards.php
+++ e-commerce-dashboards.php
@@ -44,6 +44,7 @@ function dashboards() {
     <?php
     $conn->close();
 	echo "<div id='table_div'></div>";
+	
 }
 
 // Page Settings
@@ -55,4 +56,35 @@ function settings() {
 			<label><input type='checkbox' name='customer-overview'>Customer Overview</label><br><br>
 			<input type='submit' name='submit' value='Save' class='button button-primary button-large'>
 		</form>";
+}
+
+// Add theme info box into WP Dashboard
+function insideweb_add_dashboard_widgets() {
+	wp_add_dashboard_widget('wp_dashboard_widget', 'Products', 'insideweb_theme_info');
+}
+
+add_action('wp_dashboard_setup', 'insideweb_add_dashboard_widgets' );
+
+function insideweb_theme_info() { ?>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['table']});
+      google.charts.setOnLoadCallback(drawTable);
+
+      function drawTable() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Product');
+        data.addRows([
+          <?php include 'product_echo.php';?>
+        ]);
+
+        var table = new google.visualization.Table(document.getElementById('table_div'));
+
+        table.draw(data, {showRowNumber: true, width: '100%', height: '30%'});
+      }
+    </script>
+
+    <?php
+    $conn->close();
+	echo "<div id='table_div'></div>";
 }
