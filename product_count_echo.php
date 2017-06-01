5ef77cc39086aa17e7d5d0c30d7a22012188afd2
JarnoPeeters <info@insideweb.nl>
2017-06-01 14:19:58 +0200

    Convert string to decimal for sorting

--- product_count_echo.php
+++ product_count_echo.php
@@ -1,19 +1,19 @@
 <?php
 	include 'conn_database.php';
 
-	$sql = "SELECT post_title, meta_value
+	$sql = "SELECT post_title, CAST(meta_value as DECIMAL(9,2)) _meta_value
 			FROM wp_postmeta, wp_posts
 			WHERE wp_posts.ID = wp_postmeta.post_id
 			AND post_status = 'publish'
 			AND post_type = 'product'
 			AND meta_key = '_price'
-			ORDER BY meta_value DESC";
+			ORDER BY _meta_value DESC";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
 	    // Output data of each row
 	    while($row = $result->fetch_assoc()) {
-	        echo "[" . $row['meta_value'] . ",],";
+	        echo "[" . $row['_meta_value'] . ",],";
 	    }
 	} else {
 	    echo "0 results";
