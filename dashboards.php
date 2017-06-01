a65853c70c56691998115ed42a18491245d79e75
JarnoPeeters <info@insideweb.nl>
2017-06-01 09:38:54 +0200

    Include Dashboards & Products as separate files

---
+++ dashboards.php
@@ -0,0 +1,5 @@
+<?php
+	function dashboards() {
+		echo "<h1>E-Commerce Dashboards</h1>";
+		echo "<p>Control your business with management dashboards</p>";
+	}
--- e-commerce-dashboards.php
+++ e-commerce-dashboards.php
@@ -1,22 +1,23 @@
 <?php
     /**
-        * Plugin Name: E-Commerce Dashboards
-        * Plugin URI: 
-        * Description: Control your business with management dashboards.
-        * Version: 0.1.1
-        * Author: Jarno Peeters & Pim Dieks
-        * Author URI:
-        * License: GPL2
+      * Plugin Name: E-Commerce Dashboards
+      * Plugin URI: 
+      * Description: Control your business with management dashboards.
+      * Version: 0.1.1
+      * Author: Jarno Peeters & Pim Dieks
+      * Author URI:
+      * License: GPL2
     */
 
     // Add Menu
     function addMenu() {
-        add_menu_page("Dashboards", "E-Commerce", 4, "dashboards", "dashboards", "dashicons-screenoptions", 2);
+      add_menu_page("Dashboards", "E-Commerce", 4, "dashboards", "dashboards", "dashicons-screenoptions", 2);
+      add_submenu_page("dashboards", "Products", "Products", 4, "products", "products");
     }
     add_action("admin_menu", "addMenu");
 
     // Page Dashboards
-    function dashboards() {
-        echo "<h1>E-Commerce Dashboards</h1>";
-        echo "<p>Control your business with management dashboards</p>";
-    }
+    include 'dashboards.php';
+
+    // Page Products
+    include 'products.php';
