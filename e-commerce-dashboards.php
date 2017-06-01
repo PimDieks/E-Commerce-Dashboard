a7b6a35dbf8614efe5a03655763182f259b36156
JarnoPeeters <info@insideweb.nl>
2017-06-01 09:25:49 +0200

    Problem headers already sent out fixed

--- e-commerce-dashboards.php
+++ e-commerce-dashboards.php
@@ -1,98 +1,22 @@
 <?php
-/**
- * Plugin Name: E-commerce Dashboards
- * Plugin URI: 
- * Description: Manage your e-commerce with management dashboards.
- * Version: 0.1.1
- * Author: Jarno Peeters & Pim Dieks
- * Author URI:
- * License: GPL2
- */
+    /**
+        * Plugin Name: E-Commerce Dashboards
+        * Plugin URI: 
+        * Description: Control your business with management dashboards.
+        * Version: 0.1.1
+        * Author: Jarno Peeters & Pim Dieks
+        * Author URI:
+        * License: GPL2
+    */
 
-// Add Menu
-function addMenu() {
-	add_menu_page("Dashboards", "Dashboards", 4, "dashboards", "dashboards", "dashicons-screenoptions");
-	add_submenu_page("dashboards", "Settings", "Settings", 4, "settings", "settings");
-}
-add_action("admin_menu", "addMenu");
+    // Add Menu
+    function addMenu() {
+        add_menu_page("Dashboards", "E-Commerce", 4, "dashboards", "dashboards", "dashicons-screenoptions", 2);
+    }
+    add_action("admin_menu", "addMenu");
 
-// Page Dashboards
-function dashboards() {
-	echo "<h1>E-Commerce Dashboards</h1>";
-	echo "<p>Welcome to E-Commerce Dashboards. Your all-in-one dashboard plugin!</p>";
-
-
- ?>
-
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-    
-      google.charts.load('current', {'packages':['table']});
-      google.charts.setOnLoadCallback(drawTable);
-
-      function drawTable() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Product');
-        data.addColumn('number', 'Price');
-        data.addRows([
-          <?php include 'product_echo.php';?>
-        ]);
-
-        var table = new google.visualization.Table(document.getElementById('table_div'));
-
-        table.draw(data, {showRowNumber: false, width: '30%', height: '30%'});
-        table.draw(data, {showRowNumber: true, width: '50%', height: '100%'});
-      }
-    </script>
-
-<?php
-
-    $conn->close();
-    echo "<div id='table_div'></div>";
-	
-}
-
-
-
-// Page Settings
-function settings() {
-	echo "<h1>Settings</h1>";
-	echo "<p>Select the dashboards you want to see in your overview.</p>";
-	echo "<form>
-			<label><input type='checkbox' name='product-overview'>Product Overview</label><br><br>
-			<label><input type='checkbox' name='customer-overview'>Customer Overview</label><br><br>
-			<input type='submit' name='submit' value='Save' class='button button-primary button-large'>
-		</form>";
-}
-
-// Add theme info box into WP Dashboard
-function insideweb_add_dashboard_widgets() {
-	wp_add_dashboard_widget('wp_dashboard_widget', 'Products', 'insideweb_theme_info');
-}
-
-add_action('wp_dashboard_setup', 'insideweb_add_dashboard_widgets' );
-
-function insideweb_theme_info() { ?>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['table']});
-      google.charts.setOnLoadCallback(drawTable);
-
-      function drawTable() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Product');
-        data.addRows([
-          <?php include 'product_echo.php';?>
-        ]);
-
-        var table = new google.visualization.Table(document.getElementById('table_div'));
-
-        table.draw(data, {showRowNumber: true, width: '100%', height: '30%'});
-      }
-    </script>
-
-    <?php
-    
-    $conn->close();
-	  echo "<div id='table_div'></div>";
-}
+    // Page Dashboards
+    function dashboards() {
+        echo "<h1>E-Commerce Dashboards</h1>";
+        echo "<p>Control your business with management dashboards</p>";
+    }
